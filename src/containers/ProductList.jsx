37fae43527c33d803525cbdd974bdfe7849fe03f
Bábaro Javier Valmaseda Vázquez <bjvalmaseda.g@gmail.com>
2022-06-29 19:07:50 -0400

    :sparkles: [ADD] Product list
    add axios and babel/plugin-transform-runtime to async functions

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import ProductItem from '../components/ProductItem';
-import '../styles/ProductList.scss';
+import React, { useEffect, useState } from "react";
+import ProductItem from "../components/ProductItem";
+import axios from "axios";
+import "../styles/ProductList.scss";
+
+const API = "https://api.escuelajs.co/api/v1/products";
 
 const ProductList = () => {
-	return (
-		<section className="main-container">
-			<div className="ProductList">
-				<ProductItem />
-			</div>
-		</section>
-	);
-}
+  const [products, setProducts] = useState([]);
+  useEffect(async () => {
+    const response = await axios.get(API);
+    setProducts(response.data);
+  }, []);
+  return (
+    <section className="main-container">
+      <div className="ProductList">
+        {products.map((products) => (
+          <ProductItem />
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default ProductList;
